593216762a9b87f0f0f3b33da340ace83a2dfa6d
kars130700 <karstenbergeo@gmail.com>
2024-10-06 23:40:47 +0200

    trained models

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from state_machine_code import StateMachine
 
 if __name__ == '__main__':
     restaurant_info = pd.read_csv('data/restaurant_info_expanded.csv')
-    SM = StateMachine(restaurant_info, "./models/lr_we_classifier.keras")
+    SM = StateMachine(restaurant_info, "./models/lr_bow_classifier.keras")
